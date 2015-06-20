777191a311a4515e9b783bca799775ffaacc2311
David Garber <davidgarber@gmail.com>
2015-06-20 09:59:32 -0700

    Not link to answer form. Taking a break.

--- app/models/question.js
+++ app/models/question.js
@@ -3,5 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   description: DS.attr('string'),
-  body: DS.attr('string')
+  body: DS.attr('string'),
+
+  answers: DS.hasMany('answer', {async: true})
 });
--- app/router.js
+++ app/router.js
@@ -8,7 +8,9 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   this.route("about");
   this.resource('questions', {path: '/'});
-  this.resource('question', {path: 'questions/:question_id'});
+  this.resource('question', {path: 'questions/:question_id'}, function(){
+    this.route('new-answer');
+  });
   this.route('new-question');
 });
 
